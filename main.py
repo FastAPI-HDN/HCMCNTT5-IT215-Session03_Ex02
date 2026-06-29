8643da59d98aa201b3ff3cfbc7ee74dbd5a82c5f
Hoang Duong Nam <hoangduongnampb.2k6@gmail.com>
2026-06-29 14:08:50 +0700

    bai02

---
+++ main.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI
+
+app = FastAPI(title="Library API - Lesson 2")
+
+books = [
+    {
+        "id": 1,
+        "title": "Python Basic",
+        "author": "Lê Minh Thu",
+        "category": "programming",
+        "year": 2022,
+        "is_available": True
+    },
+    {
+        "id": 2,
+        "title": "Web API Design",
+        "author": "Phạm Lan Hồng",
+        "category": "web",
+        "year": 2021,
+        "is_available": False
+    },
+    {
+        "id": 3,
+        "title": "Database System",
+        "author": "Lê Minh Huyền",
+        "category": "database",
+        "year": 2020,
+        "is_available": True
+    },
+    {
+        "id": 4,
+        "title": "Clean Code",
+        "author": "Lê Ánh Linh",
+        "category": "programming",
+        "year": 2008,
+        "is_available": False
+    }
+]
+
+@app.get("/health")
+def check_health():
+    return {"message": "Library API is running"}
+
+
+@app.get ("/books")
+def get_available_books():
+    result_books = []
+    
+    for book in books:
+        if book["is_available"] == True:
+            result_books.append(book)
+            
+    return result_books
+
+@app.get ("/books/borrowed")
+def get_borrowed_books ():
+    result_books = [book for book in books if book ["is_available"] == False]
+    return result_books
